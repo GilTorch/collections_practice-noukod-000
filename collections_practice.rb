26fbc9edcdc55be816a8cfd571650be4fd58da1a
Gilbert Torchon <gilbert@noukod.com>
2018-09-20 13:55:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,5 +3,13 @@ def sort_array_asc(number_array)
 end
 
 ef sort_array_dsc(number_array)
-  number_array.sort
+  number_array.sort do |a,b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    else
+      1
+    end
+  end
 end
