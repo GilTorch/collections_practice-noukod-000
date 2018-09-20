4afc5a25047f55abc9ff017f67059b33cd14c145
Gilbert Torchon <gilbert@noukod.com>
2018-09-20 13:57:03 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(number_array)
   number_array.sort
 end
 
-ef sort_array_dsc(number_array)
+def sort_array_dsc(number_array)
   number_array.sort do |a,b|
     if a == b
       return 0
