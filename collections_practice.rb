d83e08bd7ce937aef897c7a5bfc1a48ce6edd5b5
Gilbert Torchon <gilbert@noukod.com>
2018-09-20 14:00:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(number_array)
   number_array.sort
 end
 
-def sort_array_dsc(number_array)
+def sort_array_desc(number_array)
   number_array.sort do |a,b|
     if a == b
       0
